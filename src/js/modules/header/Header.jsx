55a457061c6c4b26db6af29d1cf6c021e7ec0422
Habu-Kagumba <habukagumba@gmail.com>
2016-01-22 20:42:02 +0300

    Add navigation stubs and svg-animations for the home logo

--- src/js/modules/header/Header.jsx
+++ src/js/modules/header/Header.jsx
@@ -1,12 +1,13 @@
+import 'styles/modules/header/navigation/_navigation'
+
 import React, {Component} from 'react'
 import Navigation from 'js/modules/header/navigation/Navigation'
 
 class Header extends Component {
   render () {
     return (
-      <header>
-        Header here
-        <Navigation />
+      <header className='header-nav'>
+        <Navigation/>
       </header>
     )
   }
--- src/js/modules/header/navigation/Navigation.jsx
+++ src/js/modules/header/navigation/Navigation.jsx
@@ -1,16 +1,39 @@
 import React, {Component} from 'react'
-import Nav from 'data/nav.json'
+import InlineSvg from 'js/modules/common/InlineSvg'
+import HSAnimation from 'js/modules/header/navigation/HSAnimation'
 
 class Navigation extends Component {
+  componentDidMount () {
+    let hs = new HSAnimation()
+    hs.initiateHS()
+    setTimeout(() => {
+      hs.animateHS()
+    }, 3000)
+  }
+  animHs () {
+    let hs = new HSAnimation()
+    hs.animateHSNodes()
+  }
+  initHS () {
+    let hs = new HSAnimation()
+    hs.initHSNodes()
+  }
   render () {
     return (
-      <div className='navigation'>
-        <ul className='navbar'>{
-          Nav.map(link => {
-            return <li className={link.align} key={link.title}>{link.title}</li>
-          })
-        }</ul>
-      </div>
+      <nav>
+            <ul className='navbar'>
+              <li className='home-svg align-left'>
+                <a href='' onMouseEnter={this.animHs} onMouseLeave={this.initHS}>
+                  <InlineSvg
+                    name='hs-logo'
+                    />
+                </a>
+              </li>
+              <li className='align-center'><a href=''>About</a></li>
+              <li className='align-center'><a href=''>Playground</a></li>
+              <li className='align-center'><a href=''>Contact</a></li>
+            </ul>
+      </nav>
     )
   }
 }
