fa97bcc66789926a6071cddecf1cb254302cd4b6
Herbert Kagumba <habukagumba@gmail.com>
2016-01-24 22:22:45 +0300

    add my image and associated animations and styling

--- src/js/modules/about/AboutPage.jsx
+++ src/js/modules/about/AboutPage.jsx
@@ -2,15 +2,29 @@ import 'styles/modules/about/_aboutpage'
 
 import React, {Component} from 'react'
 import InlineSvg from 'js/modules/common/InlineSvg'
+import MeAnimation from 'js/modules/about/MeAnimation'
 
 class About extends Component {
+  componentDidMount () {
+    let meAnim = new MeAnimation()
+    meAnim.lineMeUp()
+  }
+  handleMouseEnter () {
+    let meAnim = new MeAnimation()
+    meAnim.colorMeUp()
+  }
+  handleMouseLeave () {
+    let meAnim = new MeAnimation()
+    meAnim.lineMeUp()
+  }
   render () {
     return (
       <section className='about'>
-        <section className='me-svg'>
+        <section className='me-svg' onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>
           <InlineSvg name='me'/>
         </section>
         <section className='about-me'>
+          <h2>About Me</h2>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </section>
       </section>
